76078f1c5c29aee38ed36157de2a20c35f476a87
FastEddy1 <edwardrmock@gmail.com>
2022-06-21 15:38:21 -0600

    logic

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,93 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer'; //Import inquirer package
+import fs from 'file-system'; //Import file-system package
+import generateMarkdown from './utils/generateMarkdown.js'; //Import generateMarkdown function
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+        validate: function (value) { return value.length > 0 ? true : "Please enter a title"; }
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a short description explaining the what, why, and how of your project.",
+        validate: function (value) { return value.length > 0 ? true : "Please enter a short description"; }
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please enter instructions on how to install your project.",
+        validate: function (value) { return value.length > 0 ? true : "Please enter instructions on how to install your project"; }
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please enter instructions on how to use your project.",
+        validate: function (value) { return value.length > 0 ? true : "Please enter instructions on how to use your project"; }
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please enter instructions on how to run tests for your project.",
+        validate: function (value) { return value.length > 0 ? true : "Please enter instructions on how to run tests for your project"; }
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please enter instructions on how to contribute to your project.",
+        validate: function (value) { return value.length > 0 ? true : "Please enter instructions on how to contribute to your project"; }
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Please enter instructions on how to contact you for any questions.",
+        validate: function (value) { return value.length > 0 ? true : "Please enter instructions on how to contact you for any questions"; }
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: [
+            "GNU AGPLv3",
+            "GNU GPLv3",
+            "GNU LGPLv3",
+            "Mozilla Public License 2.0",
+            "Apache License 2.0",
+            "MIT License",
+            "Boost Software License 1.0",
+            "The Unlicense",
+            "None"
+        ],
+    }
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//Use file system to create a folder called output in the current directory and then create a folder called ${title} in the output folder
+function setupFolder() {
+    fs.mkdirSync('./output');
+    fs.mkdirSync('./output/' + title);
+}
+
+//Use file system to create a markdown file called README.md in the newly created folder called ${title} in the output folder
+function writeFile(data) {
+    fs.writeFileSync('./output/' + title + '/README.md', data);
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+//Use inquirer to prompt the user for all of the questions in the questions array and then use the generateMarkdown function to generate the README.md file then use setupFolder and writeFile to write the file to the correct folder.
+function init() {
+    inquirer.prompt(questions).then(function (data) {
+        const readme = generateMarkdown(data);
+        setupFolder();
+        writeFile(readme);
+    }
+    );
+}
 
-// Function call to initialize app
-init();
+init()
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,82 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license === "GNU AGPLv3") {
+      return licenseBadge("AGPLv3", "https://img.shields.io/badge/License-AGPLv3-blue.svg");
+    } else if (license === "GNU GPLv3") { return licenseBadge("GPLv3", "https://img.shields.io/badge/License-GPLv3-blue.svg");
+    } else if (license === "GNU LGPLv3") { return licenseBadge("LGPLv3", "https://img.shields.io/badge/License-LGPLv3-blue.svg");
+    } else if (license === "Mozilla Public License 2.0") { return licenseBadge("MPL 2.0", "https://img.shields.io/badge/License-MPL%202.0-blue.svg");
+    } else if (license === "Apache License 2.0") { return licenseBadge("Apache 2.0", "https://img.shields.io/badge/License-Apache%202.0-blue.svg");
+    } else if (license === "MIT License") { return licenseBadge("MIT", "https://img.shields.io/badge/License-MIT-blue.svg");
+    } else if (license === "Boost Software License 1.0") { return licenseBadge("BSL 1.0", "https://img.shields.io/badge/License-BSL%201.0-blue.svg");
+    } else if (license === "The Unlicense") { return licenseBadge("Unlicense", "https://img.shields.io/badge/License-Unlicense-blue.svg");
+    } else if (license === "No License") { return "";
+    } else if (license === "") { return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "GNU AGPLv3") {
+    return "https://www.gnu.org/licenses/agpl-3.0.en.html";
+  } else if (license === "GNU GPLv3") { return "https://www.gnu.org/licenses/gpl-3.0.en.html";
+  } else if (license === "GNU LGPLv3") { return "https://www.gnu.org/licenses/lgpl-3.0.en.html";
+  } else if (license === "Mozilla Public License 2.0") { return "https://www.mozilla.org/en-US/MPL/2.0/";
+  } else if (license === "Apache License 2.0") { return "https://www.apache.org/licenses/LICENSE-2.0";
+  } else if (license === "MIT License") { return "https://opensource.org/licenses/MIT";
+  } else if (license === "Boost Software License 1.0") { return "https://www.boost.org/LICENSE_1_0.txt";
+  } else if (license === "The Unlicense") { return "https://unlicense.org/";
+  } else if (license === "No License") { return "";
+  } else if (license === "") { return "";
+}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "GNU AGPLv3") {
+    return licenseSection("AGPLv3", "https://www.gnu.org/licenses/agpl-3.0.en.html");
+  } else if (license === "GNU GPLv3") { return licenseSection("GPLv3", "https://www.gnu.org/licenses/gpl-3.0.en.html");
+  } else if (license === "GNU LGPLv3") { return licenseSection("LGPLv3", "https://www.gnu.org/licenses/lgpl-3.0.en.html");
+  } else if (license === "Mozilla Public License 2.0") { return licenseSection("MPL 2.0", "https://www.mozilla.org/en-US/MPL/2.0/");
+  } else if (license === "Apache License 2.0") { return licenseSection("Apache 2.0", "https://www.apache.org/licenses/LICENSE-2.0");
+  } else if (license === "MIT License") { return licenseSection("MIT", "https://opensource.org/licenses/MIT");
+  } else if (license === "Boost Software License 1.0") { return licenseSection("BSL 1.0", "https://www.boost.org/LICENSE_1_0.txt");
+  } else if (license === "The Unlicense") { return licenseSection("Unlicense", "https://unlicense.org/");
+  } else if (license === "No License") { return "";
+  } else if (license === "") { return "";
+}
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+  ${renderLicenseBadge(data.license)}
+  ${renderLicenseLink(data.license)}
+  ${renderLicenseSection(data.license)}
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Tests](#tests)
+  * [Contributing](#contributing)
+  * [Questions](#Questions)
+  * [License](#license)
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Tests
+  ${data.tests}
+  ## Contributing
+  ${data.contributing}
+  ## Questions
+  ${data.questions}
+  ## License
+  ${data.license}
+  `;
 }
 
-module.exports = generateMarkdown;
+export default generateMarkdown;
